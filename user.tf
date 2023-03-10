074d37c42a7abb0eac44e5bea3f17107d94dbeae
sudharaka0829 <126379932+sudharaka0829@users.noreply.github.com>
2023-03-10 11:25:33 +0530

    Create user.tf

---
+++ user.tf
@@ -0,0 +1,27 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.16"
+    }
+  }
+
+  required_version = ">= 1.2.0"
+}
+
+provider "aws" {
+  region  = "us-west-2"
+}
+
+resource "aws_iam_user" "test" {
+  name = "test"
+  path = "/test/"
+}
+
+resource "aws_iam_access_key" "test" {
+  user = aws_iam_user.test.name
+}
+
+output "aws_iam_smtp_password_v4" {
+  value = nonsensitive(aws_iam_access_key.test.ses_smtp_password_v4)
+}
